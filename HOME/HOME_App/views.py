e50973fe4d10e1f15d89520a5222bbb05c93773f
Dani Cantu <daniellecantu@dovahkiin.myfiosgateway.com>
2022-12-14 00:37:29 -0500

    updating Capstone

---
+++ HOME/HOME_App/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+# Create your views here.
